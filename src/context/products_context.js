956ee950adbfccbe6c113f489967607a89efb8c4
EjikeII <ejix22@gmail.com>
2023-08-21 18:39:00 +0100

    Worked on Fetch product from API  using Axios

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -14,7 +14,11 @@ import {
 } from '../actions'
 
 const initialState = {
-  isSidebarOpen:false
+  isSidebarOpen:false,
+  products_loading:false,
+  products_error:false,
+  products:[],
+  featured_products:[],
 }
 
 const ProductsContext = React.createContext()
@@ -26,8 +30,22 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_OPEN })
   }
   const sidebarClose = () => {
-    dispatch({ type: SIDEBAR_CLOSE})
+    dispatch({ type: SIDEBAR_CLOSE })
   }
+  const fetchProducts = async(url) => {
+    dispatch({type: GET_PRODUCTS_BEGIN})
+    try {
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload:products })
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR })
+    }
+  }
+useEffect(() => {
+  fetchProducts(url)
+}, [])
+
   return (
     <ProductsContext.Provider 
     value={{ 
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -10,11 +10,31 @@ import {
 } from '../actions'
 
 const products_reducer = (state, action) => {
-  if(action.type === 'SIDEBAR_OPEN') {
+  if(action.type === SIDEBAR_OPEN) {
     return {...state, isSidebarOpen: true }
   }
-  if (action.type === 'SIDEBAR_CLOSE') {
+  if (action.type === SIDEBAR_CLOSE) {
     return {...state, isSidebarOpen: false }
+  }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return {...state, products_loading:true }
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true)
+
+  return {
+    ...state, 
+    products_loading:false,
+    products:action.payload,
+    featured_products,
+    }
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return{...state,
+      products_loading: false,
+      product_error:true
+    }
   }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
